b3a24b8f7d63308dce3311090adbf67e68cdad40
JonathanMRuiz <jonimrz@yahoo.com>
2025-03-21 17:09:40 -0300

    fiber-fluent changes in home

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const Section = ({ children, id, className }) => (
   <motion.section
     id={id}
-    className={`bg-white py-8 px-6 ${className}`}
+    className={`bg-white py-12 px-6 ${className}`}
     initial={{ opacity: 0, y: 50 }}
     whileInView={{ opacity: 1, y: 0 }}
     viewport={{ once: true }}
