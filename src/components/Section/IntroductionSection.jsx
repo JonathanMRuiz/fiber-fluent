dc1d5690d1ae74a8978c94453cc99483db41bd13
JonathanMRuiz <jonimrz@yahoo.com>
2025-03-24 19:36:49 -0300

    changes in home

--- src/components/Section/IntroductionSection.jsx
+++ src/components/Section/IntroductionSection.jsx
@@ -1,25 +1,35 @@
 import Section from './Section';
+import Intro from '../../assets/introduction.jpg';
 
 const IntroductionSection = () => (
-  <Section id="introduction">
-    <h2 className="text-4xl font-extrabold mb-6 text-[#17254E] text-center">Introduction</h2>
-    <p className="text-lg text-[#17254E] mb-6">
-      At <strong className="font-bold">Fiber Fluent LLC</strong>, we are proud to be a veteran-owned underground infrastructure company specializing in communications and electrical installations. Founded by <strong className="font-bold">Peter Merlo</strong>, a former member of the 173rd Airborne Combat Team and a seasoned industry professional with over a decade of experience in construction and land development, our company was built on a foundation of integrity, precision, and hard-earned expertise.
-    </p>
-    <p className="text-lg text-[#17254E] mb-6">
-      With deep roots in underground utilities and community development, Fiber Fluent delivers tailored solutions for <strong className="font-bold">primary, secondary, and street lighting infrastructure</strong>, as well as <strong className="font-bold">communications conduit systems</strong>. We support residential, commercial, and municipal projects with services designed to meet modern demands for power and connectivity.
-    </p>
-    <p className="text-lg text-[#17254E] mb-6">
-      Our experienced crews are equipped to handle <strong className="font-bold">OpenTrench, MissileBore, DirectionalDrill, and MicroTrench</strong> installations, ensuring the right approach for every project. From <strong className="font-bold">fiberglass light pole installations up to 20 feet tall to pedestal and handhole placements</strong>, we provide clean, efficient pathways that prepare communities for the future.
-    </p>
-    <p className="text-lg text-[#17254E] mb-6">
-      At Fiber Fluent, we believe in doing the job right the first time—on schedule and within budget. Our hands-on leadership, technical knowledge, and mission-first mindset set us apart in the industry.
-    </p>
-    <p className="text-lg text-[#17254E]">
-      <strong>Growing to Serve You Better</strong>
-      <br />
-      As Fiber Fluent continues to grow, we’re committed to expanding our services to meet the evolving needs of our clients and industry. Stay connected with us for updates on future service offerings.
-    </p>
+  <Section 
+    id="introduction" 
+    className="text-white py-16 bg-cover bg-center" 
+  >
+    <div 
+      className="bg-[#012547] sm:bg-cover sm:bg-center p-8 rounded-lg ml-8" 
+      style={{ backgroundImage: `url(${Intro})` }}
+    >
+      <div className="text-left pt-24 sm:w-[45rem] w-full">
+        <p className="text-lg mb-6">
+          At <strong className="font-bold">Fiber Fluent LLC</strong>, we are proud to be a veteran-owned underground infrastructure company specializing in communications and electrical installations. Founded by <strong className="font-bold">Peter Merlo</strong>, a former member of the 173rd Airborne Combat Team and a seasoned industry professional with over a decade of experience in construction and land development, our company was built on a foundation of integrity, precision, and hard-earned expertise.
+        </p>
+        <p className="text-lg mb-6">
+          With deep roots in underground utilities and community development, Fiber Fluent delivers tailored solutions for <strong className="font-bold">primary, secondary, and street lighting infrastructure</strong>, as well as <strong className="font-bold">communications conduit systems</strong>. We support residential, commercial, and municipal projects with services designed to meet modern demands for power and connectivity.
+        </p>
+        <p className="text-lg mb-6">
+          Our experienced crews are equipped to handle <strong className="font-bold">OpenTrench, MissileBore, DirectionalDrill, and MicroTrench</strong> installations, ensuring the right approach for every project. From <strong className="font-bold">fiberglass light pole installations up to 20 feet tall to pedestal and handhole placements</strong>, we provide clean, efficient pathways that prepare communities for the future.
+        </p>
+        <p className="text-lg mb-6">
+          At Fiber Fluent, we believe in doing the job right the first time—on schedule and within budget. Our hands-on leadership, technical knowledge, and mission-first mindset set us apart in the industry.
+        </p>
+        <p className="text-lg">
+          <strong>Growing to Serve You Better</strong>
+          <br />
+          As Fiber Fluent continues to grow, we’re committed to expanding our services to meet the evolving needs of our clients and industry. Stay connected with us for updates on future service offerings.
+        </p>
+      </div>
+    </div>
   </Section>
 );
 
